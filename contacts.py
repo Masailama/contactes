b91ba03c2600006e7b1e94005f5434229e5dc0d9
Didac Gomez <didac.programacio@gmail.com>
2022-03-25 23:25:43 +0100

    funcionalitats de l'aplicació

--- contacts.py
+++ contacts.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import ttk
-from utils import BUTTON_FONT, LABEL_FONT, PATH_PHOTO, resize_image
+from tkinter import ttk, filedialog, messagebox
+from utils import BUTTON_FONT, LABEL_FONT, PATH_PHOTO, TABLE, resize_image, run_sql
 from PIL import ImageTk
 
 
 class ContactsView(tk.Frame):
-    def __init__(self, window, db_name=""):
+    def __init__(self, window, db_name="contactes"):
         tk.Frame.__init__(self, window, bg=window.cget("bg"))
         # variables
         self.path_image = PATH_PHOTO
@@ -26,15 +26,16 @@ class ContactsView(tk.Frame):
         self.lb_city = tk.Label(self.frame_fields, text="Ciutat: ", font=LABEL_FONT, bg=self.cget("bg"))
         self.txt_city = tk.Entry(self.frame_fields, width=30, bd=0, relief="flat")
         self.txt_photo = tk.Label(self.frame_fields, image=self.img_contact)
+        self.txt_photo.bind("<Double Button 1>", self.select_image)
         self.frame_buttons = tk.Frame(self, bg=self.cget("bg"))
         self.bt_add = tk.Button(self.frame_buttons, text="Afegir registre", bg="bisque", relief="flat", bd=0, 
-            activebackground="navajo white", cursor="hand2", font=BUTTON_FONT)
+            activebackground="navajo white", cursor="hand2", font=BUTTON_FONT, command=self.add_contact)
         self.bt_update = tk.Button(self.frame_buttons, text="Actualitzar registre", bg="bisque", relief="flat", 
-            bd=0, activebackground="navajo white", cursor="hand2", font=BUTTON_FONT)
+            bd=0, activebackground="navajo white", cursor="hand2", font=BUTTON_FONT, command=self.update_contact)
         self.bt_delete = tk.Button(self.frame_buttons, text="Eliminar registre", bg="bisque", relief="flat", 
-            bd=0, activebackground="navajo white", cursor="hand2", font=BUTTON_FONT)
+            bd=0, activebackground="navajo white", cursor="hand2", font=BUTTON_FONT, command=self.delete_contact)
         self.bt_erase = tk.Button(self.frame_buttons, text="Esborrar camps", bg="bisque", relief="flat", bd=0, 
-            activebackground="navajo white", cursor="hand2", font=BUTTON_FONT)
+            activebackground="navajo white", cursor="hand2", font=BUTTON_FONT, command=self.erase_fields)
         self.frame_list = tk.Frame(self, bg=self.cget("bg"))
         self.scroll_list = tk.Scrollbar(self.frame_list, orient="vertical")
         self.contact_list = ttk.Treeview(self.frame_list, columns=["name", "surname", "phone"], height=10,
@@ -43,9 +44,14 @@ class ContactsView(tk.Frame):
         self.contact_list.heading("name", text="Nom")
         self.contact_list.heading("surname", text="Cognoms")
         self.contact_list.heading("phone", text="Telèfon")
-        self.contact_list.column("#0", width=50)
+        self.contact_list.column("#0", width=50, anchor=tk.CENTER)
+        self.contact_list.column("name", anchor=tk.CENTER)
+        self.contact_list.column("surname", anchor=tk.CENTER)
+        self.contact_list.column("phone", anchor=tk.CENTER)
+        self.contact_list.bind("<Double Button 1>", self.select_contact)
         self.scroll_list.config(command=self.contact_list.yview)
         self.draw()
+        self.create_database()
         self.txt_name.focus()
 
     def draw(self):
@@ -72,3 +78,104 @@ class ContactsView(tk.Frame):
         self.frame_list.grid(row=1, column=0, columnspan=2, pady=(10,20), padx=20)
         self.contact_list.pack(fill=tk.X, side="left")
         self.scroll_list.pack(side="right", fill=tk.Y)
+
+    def add_contact(self):
+        if self.check_fields():
+            sql = "INSERT INTO CONTACTE VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
+            params = (self.fin_id(), self.txt_name.get().upper(), self.txt_surname.get().upper(), 
+                self.txt_phone.get(), self.txt_email.get(), self.txt_street.get(), self.txt_city.get().upper(),
+                self.path_image)
+            run_sql(name=self.db_name, query=sql, parameters=params)
+            messagebox.showinfo("Contactes", "Contacte afegit correctament.")
+            self.fill_list()
+            self.erase_fields()
+
+    def update_contact(self):
+        if len(self.contact_list.selection()) > 0:
+            if self.check_fields():
+                sql = "UPDATE CONTACTE SET NOM=?, COGNOMS=?, TELEFON=?, EMAIL=?, CARRER=?, CIUTAT=?, FOTO=? WHERE ID=?"
+                params = (self.txt_name.get().upper(), self.txt_surname.get().upper(), self.txt_phone.get(), 
+                    self.txt_email.get(), self.txt_street.get(), self.txt_city.get().upper(), self.path_image,
+                    self.contact_list.item(self.contact_list.selection()[0])["text"])
+                run_sql(name=self.db_name, query=sql, parameters=params)
+                messagebox.showinfo("Contactes", "Has actualitzat un contacte.")
+                self.erase_fields()
+                self.fill_list()
+
+    def delete_contact(self):
+        if len(self.contact_list.selection()) > 0:
+            sql = "DELETE FROM CONTACTE WHERE ID=?"
+            params = (self.contact_list.item(self.contact_list.selection()[0])["text"], )
+            run_sql(name=self.db_name, query=sql, parameters=params)
+            messagebox.showinfo("Contactes", "Has eliminat un contacte.")
+            self.erase_fields()
+            self.fill_list()
+
+    def erase_fields(self):
+        self.txt_name.delete(0, tk.END)
+        self.txt_surname.delete(0, tk.END)
+        self.txt_phone.delete(0, tk.END)
+        self.txt_email.delete(0, tk.END)
+        self.txt_street.delete(0, tk.END)
+        self.txt_city.delete(0, tk.END)
+        self.add_image(PATH_PHOTO)
+        self.txt_name.focus()
+
+    def add_image(self, image):
+        self.path_image = image
+        self.img_contact = ImageTk.PhotoImage(resize_image(self.path_image))
+        self.txt_photo["image"] = self.img_contact
+
+    def select_image(self, event):
+        image =filedialog.askopenfilename(title="Seleccionar imatge", 
+            filetypes=(("imatges PNG", "*.png"), ("imatges JPG", "*.jpg;*.jpeg")))
+        if image:
+            self.add_image(image)
+
+    def create_database(self):
+        sql = TABLE
+        run_sql(name=self.db_name, query=sql)
+        self.fill_list()
+
+    def check_fields(self):
+        if self.txt_name.get() != "" and self.txt_phone.get() != "":
+            return True
+        messagebox.showerror("Contactes", "Has d'emplenar el nom i el telèfon.")
+        return False
+
+    def fin_id(self):
+        num_id = 1
+        sql = "SELECT ID FROM CONTACTE ORDER BY ID ASC"
+        data = run_sql(name=self.db_name, query=sql, read=True)
+        for item in data:
+            if item[0] != num_id:
+                break
+            num_id += 1
+        return num_id
+
+    def fill_list(self):
+        for item in self.contact_list.get_children():
+            self.contact_list.delete(item)
+        sql = "SELECT ID, NOM, COGNOMS, TELEFON FROM CONTACTE ORDER BY ID ASC"
+        data = run_sql(name=self.db_name, query=sql, read=True)
+        for item in data:
+            text = item[0]
+            values = (item[1], item[2], item[3])
+            self.contact_list.insert("", tk.END, text=text, values=values)
+
+    def select_contact(self, event):
+        if self.contact_list.item(self.contact_list.selection()[0])["text"]:
+            self.fill_fields(self.contact_list.item(self.contact_list.selection()[0])["text"])
+
+    def fill_fields(self, id_contact):
+        self.erase_fields()
+        sql = "SELECT * FROM CONTACTE WHERE ID=?"
+        params = (id_contact, )
+        data = run_sql(name=self.db_name, query=sql, parameters=params, read=True)
+        self.txt_name.insert(0, data[0][1])
+        self.txt_surname.insert(0, data[0][2])
+        self.txt_phone.insert(0, data[0][3])
+        self.txt_email.insert(0, data[0][4])
+        self.txt_street.insert(0, data[0][5])
+        self.txt_city.insert(0, data[0][6])
+        self.add_image(data[0][7])
--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ TITLE_FONT = ("Segoe UI", 18, "bold")
 LABEL_FONT = ("Segoe UI", 12, "bold")
 BUTTON_FONT = ("Segoe UI", 10, "bold")
 
+# Variables
+TABLE = """CREATE TABLE IF NOT EXISTS CONTACTE (ID INTEGER PRIMARY KEY, NOM TEXT, COGNOMS TEXT, TELEFON TEXT, EMAIL TEXT, CARRER TEXT, CIUTAT TEXT, FOTO TEXT)""" 
+
 # functions
 def resize_image(path_image, size=(100, 100)):
     image = Image.open(path_image)
